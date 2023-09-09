3681ae14705e7c15e24d8c24e319e1ea187ab443
pcsishun <pcsishun@gmail.com>
2023-09-09 16:20:31 +0700

    update

--- lib/avatar/widgetDrawerHeader.dart
+++ lib/avatar/widgetDrawerHeader.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+// import '../view/viewSetting.dart';
+// import '../main.dart';
+// import 'package:image_picker/image_picker.dart';
 // import 'package:file_selector/file_selector.dart';
 
 class WidgetDrawerHeader extends StatefulWidget {
@@ -9,63 +11,66 @@ class WidgetDrawerHeader extends StatefulWidget {
   State<WidgetDrawerHeader> createState() => _WidgetDrawerHeaderState();
 }
 
-class _WidgetDrawerHeaderState extends State<WidgetDrawerHeader>{
-  ImagePicker picker = ImagePicker();
-  XFile? image;
+class _WidgetDrawerHeaderState extends State<WidgetDrawerHeader> {
+  // ImagePicker picker = ImagePicker();
+  // XFile? image;
 
-
-  Future _getFromGallery() async {
-    final image = await picker.pickImage(source: ImageSource.gallery);
-    if (image != null) {
-      final imageTemp = XFile(image.path);
-      setState(() {
-        this.image = imageTemp;
-      });
-    }
-  }
+  // Future _getFromGallery() async {
+  //   final image = await picker.pickImage(source: ImageSource.gallery);
+  //   if (image != null) {
+  //     final imageTemp = XFile(image.path);
+  //     setState(() {
+  //       this.image = imageTemp;
+  //     });
+  //   }
+  // }
+  // void _haddleSetting() {
+  //   ViewSetting(title: "Setting");
+  // }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
+      // decoration: BoxDecoration(border:Border.all(color: Colors.blueAccent) ),
       child: Row(
-        // mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            child: ElevatedButton(
-
-              style: ElevatedButton.styleFrom(
-                shadowColor: Colors.transparent,
-                // shadowColor: null,
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(24),
+          children: [
+            Container(
+              width: 65.0,
+              height: 65.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: const Color.fromARGB(255, 255, 255, 255),
+                // border:Border.all(color: Colors.blueAccent) 
+                ),
+              margin: EdgeInsets.only(right: 20.0,),
+              // child: const Icon(Icons.person, color: Color.fromARGB(255, 68, 68, 68),size: 30.0,),
+              child: const CircleAvatar(
+                backgroundImage: const AssetImage('assets/images/pie.png'),
+                radius: 220,
               ),
-              onPressed: (() => {
-                _getFromGallery()
-              }),
-
-              child: image == null ? 
-              Container(
-                width: 25.0,
-                height: 25.0,
-                child:  Icon(Icons.person, color: Colors.blueGrey[500],),
-              )
-                : Image.file(
-                  File(image!.path),
-                  width: 25.0,
-                  height: 25.0,
-                )
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                child: Text("Firstname LastName", style: TextStyle(fontSize: 12.0,fontWeight: FontWeight.bold, color: Colors.grey[600])),
-              )
-            ],
-          )
-        ]),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  // margin: const EdgeInsets.only( right: 15.0),
+                  child: Text("Firstname",
+                      style: TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[600])),
+                ),
+                Container(
+                  // margin: const EdgeInsets.only( right: 15.0),
+                  child: Text("Lastname",
+                      style: TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[600])),
+                )
+              ],
+            )
+          ]),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -36,42 +36,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Widget>_pages = [
-    ViewHomePage(title: "Home"),
-    ViewDashboard(title: "Dashboard"),
-    ViewChannel(title: "Channel Setting"),
-    ViewWellBreathSetting(title: "WellBreath Setting"),
-    ViewSetting(title: "Setting profile"),
-    ViewLoginLogout(title: "Login/Logout"),
+  final List<Widget> _pages = [
+    const ViewHomePage(title: "Home"),
+    const ViewDashboard(title: "Dashboard"),
+    const ViewChannel(title: "Channel Setting"),
+    const ViewWellBreathSetting(title: "WellBreath Setting"),
+    const ViewSetting(title: "Setting profile"),
+    const ViewLoginLogout(title: "Login/Logout"),
   ];
 
   int _pageSelection = 0;
   String _menuSelected = "home";
-  Color _selectedMenu = Color.fromARGB(255, 252, 209, 250);
-  Icon _selectedIcon = Icon(Icons.home);
+  Color _selectedMenu = const Color.fromARGB(255, 252, 209, 250);
+  Icon _selectedIcon = const Icon(Icons.home);
 
-
-  void _haddleSelectionPage(int selector){
+  void _haddleSelectionPage(int selector) {
     setState(() {
       _pageSelection = selector;
-      if(selector == 0){
+      if (selector == 0) {
         _menuSelected = "home";
-        _selectedIcon = Icon(Icons.home);
-      }else if(selector == 1){
+        _selectedIcon = const Icon(Icons.home);
+      } else if (selector == 1) {
         _menuSelected = "dashboard";
-        _selectedIcon = Icon(Icons.dashboard);
-      }else if(selector == 2){
+        _selectedIcon = const Icon(Icons.dashboard);
+      } else if (selector == 2) {
         _menuSelected = "channel";
-        _selectedIcon = Icon(Icons.route);
-      }else if(selector == 3){
+        _selectedIcon = const Icon(Icons.route);
+      } else if (selector == 3) {
         _menuSelected = "wb";
-        _selectedIcon = Icon(Icons.thermostat_auto);
-      }else if(selector == 4){
+        _selectedIcon = const Icon(Icons.thermostat_auto);
+      } else if (selector == 4) {
         _menuSelected = "setting";
-        _selectedIcon = Icon(Icons.settings);
-      }else if(selector == 5){
+        _selectedIcon = const Icon(Icons.settings);
+      } else if (selector == 5) {
         _menuSelected = "login";
-        _selectedIcon = Icon(Icons.exit_to_app_sharp);
+        _selectedIcon = const Icon(Icons.exit_to_app_sharp);
       }
     });
   }
@@ -85,66 +84,99 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         drawer: Drawer(
           // shadowColor: null,
-          backgroundColor: Color.fromARGB(255, 255, 243, 254),
-          width: 250.0,
+          backgroundColor: const Color.fromARGB(255, 255, 243, 254),
+          width:  250.0,
           child: Column(
             children: [
-              SizedBox(
-                width: 250.0,
-                height: 100.0,
+              Container(
+                width:  250.0,
+                height: 200.0,
+                // decoration: BoxDecoration(
+                //   border:Border.all(color: Colors.blueAccent) 
+                // ),
                 child: DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 252, 209, 250),
-                  ),
-                  child: WidgetDrawerHeader(),
-                ),
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 252, 209, 250),
+                    ),
+                    child:ElevatedButton(
+                      
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(350, 500),
+                        // animationDuration: Duration(microseconds: 0),
+                        // enableFeedback: false,m
+                        splashFactory: NoSplash.splashFactory,
+                        backgroundColor: const Color.fromARGB(255, 252, 209, 250),
+                        elevation: 0,
+                        // shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        //         RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+                                
+                        //       ),
+                      ),
+                      child: WidgetDrawerHeader(),
+                      onPressed: () => {
+                        _haddleSelectionPage(4)
+                      },
+                    )),
               ),
               ListTile(
                 // tileColor: Color.fromARGB(255, 252, 209, 250),
-                tileColor: _menuSelected == 'home'? _selectedMenu: null,
-                leading: _menuSelected == 'home'?  _selectedIcon:Icon(Icons.home_outlined),
+                tileColor: _menuSelected == 'home' ? _selectedMenu : null,
+                leading: _menuSelected == 'home'
+                    ? _selectedIcon
+                    : const Icon(Icons.home_outlined),
                 title: const Text("Home"),
                 onTap: () {
                   _haddleSelectionPage(0);
                 },
               ),
               ListTile(
-                tileColor: _menuSelected == 'dashboard'? _selectedMenu: null,
-                leading: _menuSelected == 'dashboard'? _selectedIcon:Icon(Icons.dashboard_outlined),
+                tileColor: _menuSelected == 'dashboard' ? _selectedMenu : null,
+                leading: _menuSelected == 'dashboard'
+                    ? _selectedIcon
+                    : const Icon(Icons.dashboard_outlined),
                 title: const Text("Dashboard"),
                 onTap: () {
                   _haddleSelectionPage(1);
                 },
               ),
               ListTile(
-                tileColor:  _menuSelected == 'channel'? _selectedMenu: null,
-                leading: _menuSelected == 'channel' ? _selectedIcon : Icon(Icons.route_outlined),
+                tileColor: _menuSelected == 'channel' ? _selectedMenu : null,
+                leading: _menuSelected == 'channel'
+                    ? _selectedIcon
+                    : const Icon(Icons.route_outlined),
                 title: const Text("Channel"),
                 onTap: () {
                   _haddleSelectionPage(2);
                 },
               ),
               ListTile(
-                tileColor: _menuSelected == 'wb'? _selectedMenu: null,
-                leading: _menuSelected == 'wb'? _selectedIcon : Icon(Icons.thermostat),
+                tileColor: _menuSelected == 'wb' ? _selectedMenu : null,
+                leading: _menuSelected == 'wb'
+                    ? _selectedIcon
+                    : const Icon(Icons.thermostat),
                 title: const Text("WellBreath"),
                 onTap: () {
                   _haddleSelectionPage(3);
                 },
               ),
               const Expanded(child: SizedBox()),
-              const Divider(height: 1.0, color: Color.fromARGB(255, 194, 194, 194)),
+              const Divider(
+                  height: 1.0, color: Color.fromARGB(255, 194, 194, 194)),
               ListTile(
-                tileColor: _menuSelected == 'setting'? _selectedMenu: null,
-                leading: _menuSelected == 'setting'? _selectedIcon: Icon(Icons.settings_outlined),
+                tileColor: _menuSelected == 'setting' ? _selectedMenu : null,
+                leading: _menuSelected == 'setting'
+                    ? _selectedIcon
+                    : const Icon(Icons.settings_outlined),
                 title: const Text("Setting"),
                 onTap: () {
                   _haddleSelectionPage(4);
                 },
               ),
               ListTile(
-                tileColor: _menuSelected == 'login'? _selectedMenu: null,
-                leading: _menuSelected == 'login'? _selectedIcon : Icon(Icons.exit_to_app_outlined),
+                tileColor: _menuSelected == 'login' ? _selectedMenu : null,
+                leading: _menuSelected == 'login'
+                    ? _selectedIcon
+                    : const Icon(Icons.exit_to_app_outlined),
                 title: const Text("Logout"),
                 onTap: () {
                   _haddleSelectionPage(5);
@@ -153,9 +185,8 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
         ),
-        body:Container(
+        body: Container(
           child: _pages[_pageSelection],
-        )
-      );
+        ));
   }
 }
