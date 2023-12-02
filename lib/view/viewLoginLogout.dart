ebecac0c43aff5847ec7f9c8d9fa1823e275a109
pcsishun <pcsishun@gmail.com>
2023-12-02 14:04:57 +0700

    update

--- lib/view/viewLoginLogout.dart
+++ lib/view/viewLoginLogout.dart
@@ -1,27 +1,58 @@
 import 'package:flutter/material.dart';
 
-
 class ViewLoginLogout extends StatefulWidget {
   final String title;
   const ViewLoginLogout({required this.title});
 
-  @override 
+  @override
   State<ViewLoginLogout> createState() => _ViewLoginLogoutState();
 }
 
-class _ViewLoginLogoutState extends State<ViewLoginLogout>{
-  @override 
-  Widget build(BuildContext context){
+class _ViewLoginLogoutState extends State<ViewLoginLogout> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.only(top: 50.0),
-          child: Text("Login/Logout"),
-        ),
-      )
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 30),
+                width: 300,
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: "Username"),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 30),
+                width: 300,
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: "Password"),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 30),
+                child: ElevatedButton(
+                  onPressed: () {}, child: Text("LOGIN")),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
